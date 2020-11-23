5db053c26073ee827a3dbc814a7afe6dc6b30505
doomhammerhell <64651805+doomhammerhell@users.noreply.github.com>
2020-11-23 12:44:10 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import Page from "./components/page";
+
+ReactDOM.render(<Page />, document.getElementById("root"));
